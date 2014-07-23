7009ab13ca2a81d2a008af23bf2f973570d82dab
jeanfredrik <jeanfredrik@gmail.com>
2014-07-23 09:43:45 +0200

    Added basic test

--- package.js
+++ package.js
@@ -9,3 +9,9 @@ Package.on_use(function(api, where) {
 	api.add_files('lib/jwerty/jwerty.js');
 	api.add_files('lib/export-jwerty.js');
 });
+
+Package.on_test(function (api) {
+  api.use('jwerty');
+
+  api.add_files('tests.js', ['client']);
+});
---
+++ tests.js
@@ -0,0 +1,7 @@
+Tinytest.add('jwerty - Simple Test', function (test) {
+  test.equal(
+    typeof jwerty,
+    'object',
+    'jwerty exists'
+  );
+});
